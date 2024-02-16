85584e05ac985c7e293fc49ba856ae9738b9a1a8
Adam Nordquist <apnordquist@gmail.com>
2024-02-16 09:57:14 -0700

    Initialization

---
+++ MauiApp1/Data/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Data
+{
+    internal class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; } = false;
+    }
+}
